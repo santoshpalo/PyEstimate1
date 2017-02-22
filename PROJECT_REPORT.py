f1e06bf5717f8a5c79bc49c9d4de0e414feebabe
Santosh Palo <santoshkumarpalo1967@gmail.com>
2017-02-22 10:46:02 +0530

    MEDICAL TO HARIJAN PADA ROAD.

--- PROJECT_REPORT.py
+++ PROJECT_REPORT.py
@@ -1,14 +1,12 @@
 import FunctionLibrary as fl
-EC = 'Rs.{:.2f}'.format(200000)
-ECT = '(Two lakh only)'
-NW ='''Construction of C.C. road from Fabsi Harijan Pada Bye-pass road to AWC Building
+EC = 'Rs.{:.2f}'.format(400000)
+ECT = '(Four lakh only)'
+NW ='''Construction of C.C. Bath Ghat including changing room at BADABANDHA of KAUDIAMUNDA
 '''
-HA = 'out of G.G.Y. (2016-17) head of account'
-text = '''\n\tThe road from Fabsi Harijan Pada Bye-pass road to AWC Building has to be paved with cement concrete (1:2:4) using 12 mm size c.b.g. chips crust over sub-base of C.C.(1:3:6) using 40 mm size c.b.g. metal.To make go of the rain water of the coinciding school area a hume pipe of 2.50m length of NP-2 type and 300mm dia has been provided. Hence the road has been heightened by filling sand in the subgrade in an average height of 0.30m.
+HA = 'out of 14 th C.F.C. (2016-17) head of account'
+text = '''\n\tThis Estimate consists of two nos bath ghats with C.C.(1:3:6) metal concrete and C.C.(1:2:4) chips concrete wearing coat on the treads of the steps. The tank is full of water . It is to be dewatered by using a 10 HP diesel driven PUMP. The bed is filled up with slushy soil which is to be removed by manual labour
   '''
-middle = '''\n\tThis estimate has been prepared based on Analysis of Rates 2006.
-Schedule of Rates - 2014 has been taken into Account.Prevailing Labour rates
-have been taken into account at framing of the estimate.'''
+middle = '''\n\tThis estimate has been prepared based on Analysis of Rates 2006. Schedule of Rates - 2014 has been taken into Account.Prevailing Labour rateshave been taken into account at framing of the estimate.'''
 conclusion = '''\n\tAll works will be executed as per guidance of engineer-in-charge.'''
 
 
@@ -24,10 +22,9 @@ conclusion = '''\n\tAll works will be executed as per guidance of engineer-in-ch
 if __name__ == "__main__":
     
 
-    print('\n\tThis estimate amounting to',EC,ECT)
-    print('has been framed to meet the probable expenditure towards')
-    print(NW,HA)
-    print (text)
+    print('\n\tThis estimate amounting to',EC+ECT,'has been framed to meet the probable expenditure towards',NW+HA+text)
+
+
     print(middle)
     print(conclusion)
     print(fl.signature(0,'',0,''))
--- RunningAnalysis.py
+++ RunningAnalysis.py
@@ -1,12 +1,15 @@
 9# coding=utf-8
 # coding=utf-8
 import FunctionLibrary as fl
+import items as it
 print ('''Excavation of foundation trench in hard soil including dressing
 of sides and levelling of bed etc. complete.''')
 print (fl.foundation(1),'\n')
+print(it.items['ewss'])
+print (fl.foundation(2),'\n')
 print ('''\nEarth work in hard soil including breaking of clods from 5 to 7cm
 in size and laying in layers not exceeding 30cm in height''')
-print (fl.foundation(2))
+print (fl.foundation(3))
 
 #===============================================================================
 # print (fl.gradedrcc(3))
@@ -15,11 +18,10 @@ print (fl.foundation(2))
 #===============================================================================
 print (fl.concrete(3))
 print (fl.concrete(2))
-#===============================================================================
 # print (fl.reinforcement())
 #===============================================================================
 #===============================================================================
-# print (fl.rscs(6))
+print (fl.rscs(6))
 #===============================================================================
 print (fl.rscs(3))
 #===============================================================================
@@ -30,7 +32,7 @@ print (fl.rscs(3))
 # print(fl.rscs(6))
 #===============================================================================
 #===============================================================================
-# print (fl.plaster(5))
+print (fl.plaster(5))
 # print (fl.plaster(1))
 # print (fl.plaster(2))
 # print (fl.plaster(3))
@@ -59,14 +61,14 @@ print (fl.sandfilling())
 # print(fl.road(1))
 # print(fl.road(2))
 #===============================================================================
-fl.earthwork_mechanical('''Excavation, loading, unloading and carriage by mechanical means of all
- kinds of soil, including stoney earth, gravel and moorum etc interspread
- with boulders upto 1/2 cum size with all lifts and delifts including
- trimming of slopes and bed to design section specification and as
- directed by the Engineer-in-charge within initial lead of 5.0km including
- royalty and excluding charges for loading and unlaoding etc. complete.''', 
- [['hire and running of excavator',1,14]],
- [['conveyance excluding loading & unloading',1,77.4],['royalty',1,38.4]], range(1,2,1),
-  range(1,3,1), ['description','quantity','rate'])
+# fl.earthwork_mechanical('''Excavation, loading, unloading and carriage by mechanical means of all
+#  kinds of soil, including stoney earth, gravel and moorum etc interspread
+#  with boulders upto 1/2 cum size with all lifts and delifts including
+#  trimming of slopes and bed to design section specification and as
+#  directed by the Engineer-in-charge within initial lead of 5.0km including
+#  royalty and excluding charges for loading and unlaoding etc. complete.''',
+#  [['hire and running of excavator',1,14]],
+#  [['conveyance excluding loading & unloading',1,77.4],['royalty',1,38.4]], range(1,2,1),
+#   range(1,3,1), ['description','quantity','rate'])
 print('\n\nJunior Engineer\t\t Assistant Engineer\t\tBlock Development Officer')
 print('Binka Block Ofiice\tBinka Block Office\t\t\tBinka')
